783bebbf009e97ee6c2c48af29e4e27ad3f881ce
Kirubashankar K <48342585+KIRUBASHANKAR26@users.noreply.github.com>
2021-05-05 14:56:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var newData = [];
 var xhttp = new XMLHttpRequest();
-xhttp.open("GET","http://www.filltext.com/?rows=32&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D",true);
+xhttp.open("GET",src="http://www.filltext.com/?rows=32&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D",true);
 xhttp.onreadystatechange = function() {
     if(this.readyState === 4){
         var adminData = JSON.parse(this.responseText);
